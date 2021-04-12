50c73da20d7ac6b3ce4ba1b198866e7b33e2d46f
kennethzhang <kennethzhang374@gmail.com>
2021-04-11 22:51:35 -0700

    polish add css to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useGlobalContext } from "../Context";
-
+import "../css/Dashboard.css";
 const Dashboard = () => {
 	const { getNameFromDashboard, currentUserName, logout } = useGlobalContext();
 
@@ -9,7 +9,7 @@ const Dashboard = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 	return (
-		<div>
+		<div className="dashboard-container">
 			<h1>Dashboard</h1>
 			<h1>Welcome back {currentUserName}</h1>
 			<button onClick={logout}>Logout</button>
