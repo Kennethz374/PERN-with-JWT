7abfc0631b298d226e7cdf1f0ee22f3049344962
kennethzhang <kennethzhang374@gmail.com>
2021-04-16 01:25:20 -0700

    add some basic css to the modal

--- client/src/components/AddingModal.js
+++ client/src/components/AddingModal.js
@@ -4,46 +4,50 @@ import { useGlobalContext } from "../Context";
 import "../css/AddingModal.css";
 
 const AddingModal = () => {
-	const { currentUserId, handleNewBabyInfo, newBaby } = useGlobalContext();
+	const { currentUserId, handleNewbabyInfo, newBaby } = useGlobalContext();
 	const { baby_name, baby_gender, baby_birthday, baby_owner_id } = newBaby;
 	return (
 		<div className="modal-container">
-			<form>
-				<input
-					type="text"
-					name="baby_onwer_id"
-					placeholder="baby_onwer_id"
-					value={baby_owner_id}
-					onChange={() => handleNewBabyInfo("baby_owner_id", currentUserId)}
-					className="modal_baby_onwer_id"
-				/>
-				{/* invisible */}
+			<div className="form-center">
+				<form className="baby-form">
+					<input
+						type="text"
+						name="baby_owner_id"
+						placeholder="baby_owner_id"
+						value={baby_owner_id}
+						onChange={() => handleNewbabyInfo("baby_owner_id", currentUserId)}
+						className="modal_baby_owner_id"
+					/>
+					{/* invisible */}
 
-				<input
-					type="text"
-					name="baby_name"
-					placeholder="baby_name"
-					value={baby_name}
-					onChange={(e) => handleNewBabyInfo("baby_name", e.target.value)}
-					className="modal_baby_name"
-				/>
-				<input
-					type="text"
-					name="baby_gender"
-					placeholder="baby_gender"
-					value={baby_gender}
-					onChange={(e) => handleNewBabyInfo("baby_gender", e.target.value)}
-					className="modal_baby_gender"
-				/>
-				<input
-					type="text"
-					name="baby_birthday"
-					placeholder="baby_birthday"
-					value={baby_birthday}
-					onChange={(e) => handleNewBabyInfo("baby_birthday", e.target.value)}
-					className="modal_baby_birthday"
-				/>
-			</form>
+					<input
+						type="text"
+						name="baby_name"
+						placeholder="baby_name"
+						value={baby_name}
+						onChange={(e) => handleNewbabyInfo("baby_name", e.target.value)}
+						className="modal_baby_name"
+					/>
+					<input
+						type="text"
+						name="baby_gender"
+						placeholder="baby_gender"
+						value={baby_gender}
+						onChange={(e) => handleNewbabyInfo("baby_gender", e.target.value)}
+						className="modal_baby_gender"
+					/>
+					<input
+						type="text"
+						name="baby_birthday"
+						placeholder="baby_birthday"
+						value={baby_birthday}
+						onChange={(e) => handleNewbabyInfo("baby_birthday", e.target.value)}
+						className="modal_baby_birthday"
+					/>
+
+					<button>submit</button>
+				</form>
+			</div>
 		</div>
 	);
 };
