99ed7a08d736b74d781320b82a67b341018be367
kennethzhang <kennethzhang374@gmail.com>
2021-04-16 13:06:51 -0700

    refactor baby route controller to controller file

---
+++ server/controller/babies_controller.js
@@ -0,0 +1,28 @@
+const pool = require("../db");
+export const createBaby = async (req, res, next) => {
+	const { baby_name, baby_gender, baby_birthday, baby_owner_id } = req.body;
+	try {
+		const newBaby = await pool.query(
+			"INSERT INTO babies (baby_name, baby_gender, baby_birthday, baby_owner_id) VALUES ($1,$2,$3,$4) RETURNING *",
+			[baby_name, baby_gender, baby_birthday, baby_owner_id]
+		);
+		return res.json(newBaby.rows[0]);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json("Server Error");
+	}
+};
+
+export const deleteBaby = async (req, res, next) => {
+	const { baby_id } = req.body;
+	try {
+		const removeBaby = await pool.query(
+			"DELETE FROM babies WHERE baby_id = $1",
+			[baby_id]
+		);
+		return res.json({ msg: "remove baby sucessfully" });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json("Server Error");
+	}
+};
--- server/routes/baby.js
+++ server/routes/baby.js
@@ -1,34 +1,11 @@
 const router = require("express").Router();
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
+import { createBaby, deleteBaby } from "../controller/babies_controller";
 
-router.post("/create", authorization, async (req, res, next) => {
-	const { baby_name, baby_gender, baby_birthday, baby_owner_id } = req.body;
-	try {
-		const newBaby = await pool.query(
-			"INSERT INTO babies (baby_name, baby_gender, baby_birthday, baby_owner_id) VALUES ($1,$2,$3,$4) RETURNING *",
-			[baby_name, baby_gender, baby_birthday, baby_owner_id]
-		);
-		return res.json(newBaby.rows[0]);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json("Server Error");
-	}
-});
+router.post("/create", authorization, createBaby);
 
 //verify owner_id and user_id match before removal
-router.delete("/delete", authorization, async (req, res, next) => {
-	const { baby_id } = req.body;
-	try {
-		const removeBaby = await pool.query(
-			"DELETE FROM babies WHERE baby_id = $1",
-			[baby_id]
-		);
-		return res.json({ msg: "remove baby sucessfully" });
-	} catch (err) {
-		console.error(err);
-		res.status(500).json("Server Error");
-	}
-});
+router.delete("/delete", authorization, deleteBaby);
 
 module.exports = router;
